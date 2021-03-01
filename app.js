208d4b078eb8e057449447d55df4fbcb4c35fa80
Appaji Gorrela <tejarongala@gmail.com>
2021-03-01 18:18:53 +0530

    todoV2

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ =require("lodash");
+
+mongoose.connect('mongodb+srv://itsmesiva:ursa1376@cluster0.1fpwz.mongodb.net/todoDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
 
 const app = express();
 
@@ -9,38 +16,204 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
- let newL = [];
+ 
+ var  day = date.getDate();
+
+
+const Itemschema = 
+({
+	name : 
+ 	{
+		type : String,
+		required: true
+	}
+});
+
+const Item = mongoose.model("item", Itemschema);
+
+/*const item1 = new Item(
+{
+	name:"one"
+});
+
+const item2 = new Item(
+{
+	name:"two"
+});
+
+const item3 = new Item(
+{
+	name:"three"
+});*/
+
+const club = [];
+
+
+//doc for mongoose insertion
+
+const ListSchema = {
+
+	name : String,
+	contains: [Itemschema]
+};
+
+const List = mongoose.model("list", ListSchema);
+ 
+
+
+
+// finding or accessing DB (array of items)
 
 
 app.get("/", function(req, res)
 	{
 
-let today = new Date();
-
-
-let options = 
+Item.find({}, function(err,results)
 {
 
-	weekday : "long",
-	day : "numeric",
-	month : "long"
-};
+if(results.length === 0)
+{
+	/*var  day = date.getDate();*/
 
-let day = today.toLocaleDateString("en-US", options);
+	res.render("list", {DAY : day, ents : results});
 
-
-
-res.render("list", {DAY : day, ents : newL});
 	
-	});
 
-app.post("/", function(req, res)
-{
-   let add = req.body.entry;
-  newL.push(add);
-  res.redirect("/");
+/*Item.insertMany(club, function(err) {});	
+res.redirect("/");*/
+}
+else{	/*
+
+var  day = date.getDate();*/
+
+res.render("list", {DAY : day, ents : results});
+}
+
 
 });
+});
+
+	
+
+//insertion of entry//
+app.post("/", function(req, res)
+{
+	let itemName = req.body.entry;
+	let listName = req.body.button;
+	console.log(listName);
+
+	const item4 = new Item
+	(
+	{
+		name: itemName
+
+	});
+
+	if(listName === day)
+	{
+
+		item4.save();
+
+	res.redirect("/"); 
+
+	}else
+	
+	{
+
+		List.findOne({name: listName}, function(err, foundlist)
+		{
+
+			foundlist.contains.push(item4);
+			foundlist.save();
+			res.redirect("/" + listName ); 
+
+		});
+
+
+	}
+	
+
+	});
+	
+		//custom todo list
+
+app.get("/:newDB", function(req,res)
+{
+const Ntodo = _.capitalize(req.params.newDB);
+
+
+
+
+List.findOne({name: Ntodo}, function(err, foundlist)
+
+{
+	if(!err)
+	{
+		if(!foundlist)
+		{
+			//create new todolist
+
+
+ const inventory = new List (
+ {
+ 	name: Ntodo,
+ 	contains : club
+
+ });
+
+ inventory.save();
+
+ res.redirect("/" + Ntodo);
+
+
+
+
+		}else
+		{
+			//show existing todolist 
+
+			res.render("list", {DAY : foundlist.name, ents : foundlist.contains})
+		}
+	}
+});
+
+
+});
+
+
+
+
+//deletion of each item//
+
+app.post("/delete", function(req,res)
+	{
+		let remitem =req.body.checkbox;
+		let todelete =req.body.del;
+
+		if(todelete === day)
+		{
+		Item.findByIdAndRemove(remitem, function(err){
+			if(!err)
+			{
+				console.log("deleted");
+				res.redirect("/");
+			}
+
+		});
+	}else
+	{
+		List.findOneAndUpdate({name: todelete},{$pull: {contains: {_id: remitem}}}, function(err, foundlist)
+		{
+			if(!err)
+			{
+				res.redirect("/" + todelete);
+			}
+		});
+
+
+
+	}
+	});
 
 
 app.listen(process.env.PORT || 3000, function()
